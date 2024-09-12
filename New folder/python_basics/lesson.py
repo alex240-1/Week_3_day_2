b83e69451f9b799bb1ee2e95c60dab31941f94f5
Vasquez-Garcia <avazquez-ga@cps.k12.il.us>
2024-09-12 14:45:16 -0500

    first commit

---
+++ New folder/python_basics/lesson.py
@@ -0,0 +1,23 @@
+# print("exlxllx")
+#mikdir means make directory
+#cd means change directory
+#ls means list directory
+#pwd means print working directory
+#touch means create a file
+#rm means remove a file
+# pound sign means comment
+#print("Hello World")
+#print("Alexander Vasquez")
+# print("age \t 16")
+# # \t means tab
+name = input ("what is your name")
+print("Hello," + name )
+# ask for age
+print(" You are " + age + " years old ")
+#ask for their place of birth
+#print out place of birth
+#ask for thier address
+#print out address
+#ask what school they went to
+#print out school
+age = input ("How old are you")
